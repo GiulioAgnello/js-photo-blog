3b876f0053fa78bc4db0db06c18ab0947c5b7722
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-24 16:49:30 +0200

    chiamataApi

---
+++ jv/script.js
@@ -0,0 +1,30 @@
+const postHolder = document.getElementById("postHolder");
+
+// dpbbiamo prendere le info da api
+
+apiLink = "https://lanciweb.github.io/demo/api/pictures/";
+
+// modello col
+
+// axios chiamata api
+
+axios.get(apiLink).then((respons) => {
+  const postArray = respons.data;
+  console.log(postArray[0]);
+  for (let i = 0; i < postArray.length; i++) {
+    currentPost = postArray[i];
+
+    console.log(currentPost.url);
+    const post = `<div class="col-3">
+        <div class="postcard">
+          <img class="pin" src="./img/pin.svg" alt="" />
+          <div class="cardhead"><img src="${currentPost.url}" alt="" /></div>
+          <div class="cardmain">
+            <p>${currentPost.title}</p>
+            <p>${currentPost.date}</p>
+          </div>
+        </div>
+      </div>`;
+    postHolder.innerHTML += post;
+  }
+});
