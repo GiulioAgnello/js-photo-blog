bf1bb5389d5184e35f371a1239539bca2242baae
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-28 12:15:54 +0200

    layover.layout

--- jv/script.js
+++ jv/script.js
@@ -18,7 +18,7 @@ axios.get(apiLink).then((respons) => {
     const post = `<div class="col-12 col-sm-4 col-xl-3 ">
         <div class="postcard">
           <img class="pin" src="./img/pin.svg" alt="" />
-          <div class="cardhead"><img src="${currentPost.url}" alt="" /></div>
+          <div class="cardhead"><img src="${currentPost.url}" alt="${currentPost.title}" /></div>
           <div class="cardmain">
             <p>${currentPost.title}</p>
             <p>${currentPost.date}</p>
