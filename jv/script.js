49459a1f40269e6efba38f7538a606483f4e4e2b
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-28 15:46:09 +0200

    rotation

--- jv/script.js
+++ jv/script.js
@@ -12,6 +12,7 @@ apiLink = "https://lanciweb.github.io/demo/api/pictures/";
 axios.get(apiLink).then((respons) => {
   const postArray = respons.data;
   console.log(postArray);
+  // creo tutti i post in base ai loro dati presi da axios
   for (let i = 0; i < postArray.length; i++) {
     currentPost = postArray[i];
 
@@ -20,6 +21,7 @@ axios.get(apiLink).then((respons) => {
         <div class="postcard" id="post-card-${currentPost.id}">
           
           <div class="cardhead"><img src="${currentPost.url}" alt="${currentPost.title}" /></div>
+          <img class="pin" src="./img/pin.svg" alt="">
           <div class="cardmain">
             <p>${currentPost.title}</p>
             <p>${currentPost.date}</p>
@@ -30,18 +32,20 @@ axios.get(apiLink).then((respons) => {
   }
   const postcardEl = document.querySelectorAll(".postcard");
   console.log(postcardEl);
+  // identifico ogni card per essere cliccata
   postcardEl.forEach((post) => {
     console.log(post);
 
-    // primo clic
+    // primo click
     post.addEventListener("click", (e) => {
       e.preventDefault();
       const image = document.querySelector(`#${post.id} img`).src;
-
+      // identifico l'url di ogni card e lo identfico come costante
       let overlayImage = `<img src="${image}" alt="${post.title}" /> `;
 
       imgContaner.innerHTML += overlayImage;
       const bottonEl = document.querySelector(".botton");
+      // Evento per la chiusura dell'overlay aperto
       bottonEl.addEventListener("click", (e) => {
         e.preventDefault();
         imgContaner.innerHTML = "";
